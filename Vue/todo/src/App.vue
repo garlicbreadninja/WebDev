34f135acaee232bc9215b9b83a7b0787f1c0267f
Anurag Akella <anuragakellaak@outlook.com>
2019-05-08 18:06:05 +0530

    todo-app-basic
    basic structure, no API

--- Vue/todo/src/App.vue
+++ Vue/todo/src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
+    <Header />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <AddTodo v-on:add-todo="addTodo"/>
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos';
+import Header from './components/layout/Header'
+import AddTodo from './components/AddTodo'
 export default {
-  name: 'app',
+  name: 'App',
   components: {
+    Todos, Header, AddTodo
   },
   data() {
     return {
      todos: [
        {
          id: 1,
-         title: "one",
+         title: "Todo one",
          completed: false
        },
        {
          id: 2,
-         title: "two",
+         title: "Todo two",
          completed: false
        },
        {
          id: 3,
-         title: "three",
+         title: "Todo three",
          completed: false
        }
      ]
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo];
     }
   }
 }
@@ -43,4 +58,15 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+ .btn{
+   display: inline-block;
+   border: none;
+   background: #555;
+   color: #fff;
+   padding: 7px 20px;
+   cursor: pointer;
+ }
+ .btn:hover{
+   background: #666;
+ }
 </style>
