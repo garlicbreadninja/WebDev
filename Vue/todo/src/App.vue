d508cdfcbed94045a73c81404a43334fa3a6efda
Anurag Akella <anuragakellaak@outlook.com>
2019-05-08 14:02:07 +0530

    removed helloworld garbage

--- Vue/todo/src/App.vue
+++ Vue/todo/src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+     todos: [
+       {
+         id: 1,
+         title: "one",
+         completed: false
+       },
+       {
+         id: 2,
+         title: "two",
+         completed: false
+       },
+       {
+         id: 3,
+         title: "three",
+         completed: false
+       }
+     ]
+    }
   }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
